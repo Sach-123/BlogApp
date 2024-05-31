281d343395aaafd53e181c6d0f171b7072c70d9e
Sach-123 <sachinchoudhary0203@gmail.com>
2024-05-31 13:29:38 +0530

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,16 @@
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from './Button'
+import Input from './Input'
+import RTE from './RTE'
+import Select from './Select'
+import Signup from "./Signup.jsx";
+import Login from './Login.jsx'
+import PostCard from './PostCard.jsx'
+import PostForm from './post-form/PostForm.jsx'
+import AuthLayout from './AuthLayout.jsx';
+
+export {Header, Footer, Container, Logo, LogoutBtn, Button, Input, RTE, Select, Signup, Login, PostCard, PostForm, AuthLayout}
